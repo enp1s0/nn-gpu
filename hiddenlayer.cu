4702f0e2226f26ea969708937c539c92c370461d
mutsuki <mutsuki@momo86.net>
2017-11-15 08:51:33 +0900

    add activate function

--- hiddenlayer.cu
+++ hiddenlayer.cu
@@ -2,6 +2,24 @@
 
 using namespace mtk;
 
+const int BLOCKS = 1 << 7;
+
+template<class T>
+__global__ void devicaMap(float *device_ptr_dst,float* device_ptr_src,int max_t){
+	int tid = threadIdx.x + blockIdx.x * blockDim.x;
+	if(max_t <= tid)
+		return;
+	device_ptr_dst[tid] = T()(device_ptr_src[tid]);
+}
+
+class ActId{
+public:
+	__device__ float operator()(float a) const{
+		return a;
+	}
+};
+
+
 HiddenLayer::HiddenLayer(int input_size,int output_size,int batch_size,std::string layer_name,cublasHandle_t *cublas):
 	BaseLayer(input_size,output_size,batch_size,layer_name,cublas)
 {}
@@ -13,5 +31,6 @@ void HiddenLayer::learningBackPropagation(mtk::MatrixXf &next_error, const mtk::
 }
 
 void HiddenLayer::activation(mtk::MatrixXf &output, const mtk::MatrixXf &input) const {
-
+	int matrix_size = input.getCols() * input.getRows();
+	devicaMap<ActId><<<BLOCKS,(matrix_size+BLOCKS-1)/BLOCKS>>>(output.getDevicePointer(),input.getDevicePointer(),matrix_size);
 }
--- main.cu
+++ main.cu
@@ -1,7 +1,17 @@
 #include <iostream>
 #include "cuda_common.h"
-
+#include "hiddenlayer.h"
 
 
 int main(){
+	mtk::HiddenLayer hidden0(100,100,100,"poipoi",nullptr);
+	mtk::HiddenLayer hidden1(100,100,100,"poipoi",nullptr);
+	mtk::HiddenLayer hidden2(100,100,100,"poipoi",nullptr);
+	mtk::HiddenLayer hidden3(100,100,100,"poipoi",nullptr);
+	mtk::HiddenLayer hidden4(100,100,100,"poipoi",nullptr);
+	mtk::HiddenLayer hidden5(100,100,100,"poipoi",nullptr);
+	mtk::HiddenLayer hidden6(100,100,100,"poipoi",nullptr);
+	mtk::HiddenLayer hidden7(100,100,100,"poipoi",nullptr);
+	mtk::HiddenLayer hidden8(100,100,100,"poipoi",nullptr);
+	mtk::HiddenLayer hidden9(100,100,100,"poipoi",nullptr);
 }

4bf4e9c9d46a065a6eb5e80b75c666987980399b
mutsuki <mutsuki@momo86.net>
2017-11-16 11:25:47 +0900

    softmax test

--- main.cu
+++ main.cu
@@ -2,14 +2,17 @@
 #include "cuda_common.h"
 #include "hiddenlayer.h"
 #include "matrix_array.h"
+#include "softmaxlayer.h"
 
 
 int main(){
 	cublasHandle_t cublas;
 	CUBLAS_HANDLE_ERROR(cublasCreate(&cublas));
-	mtk::HiddenLayer h0(14*14,28*28,100,"h",cublas);
-	//mtk::HiddenLayer h0(100,100,100,"h",&cublas,10.f,1.0f,1.0f);
-	for(int i = 0;i < 100;i++)
-		h0.learningReflect();
+	mtk::SoftmaxLayer s0(100,10,10,"output layer",cublas);
+	mtk::MatrixXf input,output;
+	input.setSize(100,10)->allocateDevice()->initDeviceRandom(-1.0f,1.0f);
+	output.setSize(10,10)->allocateDevice()->allocateHost()->initDeviceConstant(0.0f);
+	s0.learningForwardPropagation(output,input);
+	output.copyToHost();
 	CUBLAS_HANDLE_ERROR(cublasDestroy( cublas));
 }

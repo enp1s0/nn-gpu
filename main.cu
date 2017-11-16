54fc4792dbea0f0233debc76c160ea955fe85a15
mutsuki <mutsuki@momo86.net>
2017-11-16 12:48:27 +0900

    add network

--- main.cu
+++ main.cu
@@ -8,7 +8,7 @@ const int input_size = 28 * 28;
 const int layer0_output_size = 20 * 20;
 const int layer1_output_size = 10;
 const int batch_size = 64;
-const int calc = 20000;
+const int calc = 200;
 const int test_interval = 500;
 
 int main(){
@@ -25,12 +25,20 @@ int main(){
 	hidden0.setSize(layer0_output_size,batch_size)->allocateDevice()->initDeviceConstant(0.0f);
 	output.setSize(layer1_output_size,batch_size)->allocateDevice()->allocateHost()->initDeviceConstant(0.0f);
 
+	// error 
+	mtk::MatrixXf input_error,hidden0_error,output_error;
+	input_error.setSize(layer0_output_size,batch_size)->allocateDevice()->initDeviceConstant(0.0f);
+	hidden0_error.setSize(layer1_output_size,batch_size)->allocateDevice()->initDeviceConstant(0.0f);
+	output_error.setSize(layer1_output_size,batch_size)->allocateDevice()->initDeviceConstant(1.0f);
 	for(int c = 0;c < calc;c++){
 		// 順方向計算
 		layer0.learningForwardPropagation(hidden0,input);
 		layer1.learningForwardPropagation(output,hidden0);
-		// 逆方向計算
+		// 誤差計算
 
+		// 逆方向計算
+		layer1.learningBackPropagation(	hidden0_error, output_error);
+		layer0.learningBackPropagation( input_error, hidden0_error, layer1.getWeightPointer());
 		// 反映
 		layer0.learningReflect();
 		layer1.learningReflect();

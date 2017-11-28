eb7ad9e12bc45529947998e75277751b7054fd5b
mutsuki <mutsuki@momo86.net>
2017-11-28 10:35:25 +0900

    mv source files to src directory

--- src/main.cu
+++ src/main.cu
@@ -31,7 +31,7 @@ int main(){
 	CUBLAS_HANDLE_ERROR(cublasCreate(&cublas));
 
 	mtk::NeuralNetwork network(batch_size,cublas);
-	network.add(new mtk::HiddenUnit(input_size,network0_output_size,batch_size,"first network",cublas,1.2,1.0f,0.5f))
+	network.add(new mtk::HiddenUnit(input_size,network0_output_size,batch_size,"first network",cublas,1.8,1.0f,0.5f))
 		//->add(new mtk::HiddenUnit(network0_output_size,network1_output_size,batch_size,"second network",cublas))
 		->add(new mtk::SoftmaxUnit(network0_output_size,last_output_size,batch_size,"last network",cublas))
 		->construct();
@@ -51,12 +51,12 @@ int main(){
 	// 学習データ
 	std::cout<<"Loading training data ... ";std::cout.flush();
 	mtk::MNISTLoader mnist;
-	if(mnist.loadMNISTTrainData("train-images-idx3-ubyte","train-labels-idx1-ubyte")){
+	if(mnist.loadMNISTTrainData("../mnist/train-images-idx3-ubyte","../mnist/train-labels-idx1-ubyte")){
 		std::cout<<std::endl;
 		std::cerr<<"invalid training file name"<<std::endl;
 		return 1;
 	}
-	if(mnist.loadMNISTTestData("t10k-images-idx3-ubyte","t10k-labels-idx1-ubyte")){
+	if(mnist.loadMNISTTestData("../mnist/t10k-images-idx3-ubyte","../mnist/t10k-labels-idx1-ubyte")){
 		std::cout<<std::endl;
 		std::cerr<<"invalid training file name"<<std::endl;
 		return 1;

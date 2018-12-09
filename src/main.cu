ba170947e68d89be3d6095946791448eb8d22b2a
mutsuki <mutsuki@momo86.net>
2018-12-09 12:20:13 +0900

    update : var of MNIST data directory

--- src/main.cu
+++ src/main.cu
@@ -19,6 +19,8 @@ const int test_interval = calc * 0.01f;
 
 const int test_batch_size = 1000;
 
+const std::string data_dir = "kmnist";
+
 
 int main(){
 	std::cout<<"calc = "<<calc<<std::endl;
@@ -53,12 +55,12 @@ int main(){
 	// 学習データ
 	std::cout<<"Loading training data ... ";std::cout.flush();
 	mtk::MNISTLoader mnist;
-	if(mnist.loadMNISTTrainData("mnist/train-images-idx3-ubyte","mnist/train-labels-idx1-ubyte")){
+	if(mnist.loadMNISTTrainData(data_dir+"/train-images-idx3-ubyte",data_dir+"/train-labels-idx1-ubyte")){
 		std::cout<<std::endl;
 		std::cerr<<"invalid training file name"<<std::endl;
 		return 1;
 	}
-	if(mnist.loadMNISTTestData("mnist/t10k-images-idx3-ubyte","mnist/t10k-labels-idx1-ubyte")){
+	if(mnist.loadMNISTTestData(data_dir+"/t10k-images-idx3-ubyte",data_dir+"/t10k-labels-idx1-ubyte")){
 		std::cout<<std::endl;
 		std::cerr<<"invalid training file name"<<std::endl;
 		return 1;
